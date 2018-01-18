f08b936b0ebaf0eab5bb3acf0f9fd3b942689319
Hanaasagi <ambiguous404@gmail.com>
2018-01-18 11:13:30 +0900

    add server

--- src/server.rs
+++ src/server.rs
@@ -7,7 +7,7 @@ extern crate log;
 
 use std::cell::RefCell;
 use std::io;
-use std::net::{SocketAddr, Ipv4Addr, SocketAddrV4};
+use std::net::{SocketAddr, Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
 use std::rc::Rc;
 use std::str;
 use std::time::Duration;
@@ -155,7 +155,24 @@ impl Client {
                         let addr = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
                         let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
                         let addr = SocketAddrV4::new(addr, port);
+                        info!("proxy {}", addr);
                         (c, SocketAddr::V4(addr))
+                    }))
+                },
+                v5::ATYP_IPV6 => {
+                    mybox(read_exact(c, [0u8; 18]).map(|(conn, buf)| {
+                        let mut ip_bytes = vec![0u16; 8];
+                        for i in 0..8 {
+                            ip_bytes[i] = ((buf[2*i] as u16) << 8) | (buf[2*i+1] as u16);
+                        }
+                        let addr = Ipv6Addr::new(ip_bytes[0], ip_bytes[1],
+                                                 ip_bytes[2], ip_bytes[3],
+                                                 ip_bytes[4], ip_bytes[5],
+                                                 ip_bytes[6], ip_bytes[7]);
+                        let port = ((buf[16] as u16) << 8) | (buf[17] as u16);
+                        let addr = SocketAddrV6::new(addr, port, 0, 0);
+                        info!("proxy {}", addr);
+                        (conn, SocketAddr::V6(addr))
                     }))
                 },
                 v5::ATYP_DOMAIN => {
@@ -176,7 +193,9 @@ impl Client {
                         let port = ((buf[pos] as u16) << 8) | (buf[pos + 1] as u16);
 
                         if let Ok(ip) = hostname.parse() {
-                            return mybox(future::ok((conn, SocketAddr::new(ip, port))))
+                            let addr = SocketAddr::new(ip, port);
+                            info!("proxy {}", addr);
+                            return mybox(future::ok((conn, addr)))
                         }
 
                         let name = match Name::parse(hostname, Some(&Name::root())).map_err(|e| {
@@ -185,17 +204,9 @@ impl Client {
                             Ok(v) => v,
                             Err(e) => return mybox(future::err(e)),
                         };
-
+                        info!("proxy {}:{}", name, port);
                         let ipv4 = dns.lookup(name, port);
                         mybox(ipv4.map(|addr| (conn, addr)))
-                        //let (name, port) = match name_port(&buf) {
-                            //Ok(UrlHost::Name(name, port)) => (name, port),
-                            //Ok(UrlHost::Addr(addr)) => {
-                                //return mybox(future::ok((conn, addr)))
-                            //}
-                            //Err(e) => return mybox(future::err(e)),
-                        //};
-
                     }))
                 },
                 n => {
@@ -204,13 +215,98 @@ impl Client {
                 },
             }
         }));
+
+
+        let handle = self.handle.clone();
+        let connected = mybox(addr.and_then(move |(c, addr)| {
+            TcpStream::connect(&addr, &handle).then(move |c2| Ok((c, c2, addr)))
+        }));
+
+        let handshake_finish = mybox(connected.and_then(|(c1, c2, addr)| {
+            // response buffer
+            let mut resp = [0u8; 32];
+
+            // protocal version
+            resp[0] = 5;
+
+            // rep field
+            resp[1] = match c2 {
+                Ok(..) => 0,
+                Err(ref e) if e.kind() == io::ErrorKind::ConnectionRefused => 5,
+                Err(..) => 1,
+            };
+
+            // rsv field
+            resp[2] = 0;
+
+            let addr = match c2.as_ref().map(|r| r.local_addr()) {
+                Ok(Ok(addr)) => addr,
+                Ok(Err(..)) |
+                Err(..) => addr,
+            };
+
+            let pos = match addr {
+                SocketAddr::V4(ref a) => {
+                    resp[3] = 1;
+                    resp[4..8].copy_from_slice(&a.ip().octets()[..]);
+                    8
+                }
+                SocketAddr::V6(ref a) => {
+                    resp[3] = 4;
+                    let mut pos = 4;
+                    for &segment in a.ip().segments().iter() {
+                        resp[pos] = (segment >> 8) as u8;
+                        resp[pos + 1] = segment as u8;
+                        pos += 2;
+                    }
+                    pos
+                }
+            };
+            resp[pos] = (addr.port() >> 8) as u8;
+            resp[pos + 1] = addr.port() as u8;
+
+            // Slice our 32-byte `resp` buffer to the actual size, as it's
+            // variable depending on what address we just encoding. Once that's
+            // done, write out the whole buffer to our client.
+            //
+            // The returned type of the future here will be `(TcpStream,
+            // TcpStream)` representing the client half and the proxy half of
+            // the connection.
+            let mut w = Window::new(resp);
+            w.set_end(pos + 2);
+            write_all(c1, w).and_then(|(c1, _)| {
+                c2.map(|c2| (c1, c2))
+            })
+        }));
+
+        let timeout = Timeout::new(Duration::new(10, 0), &self.handle).unwrap();
+        let pair = mybox(handshake_finish.map(Ok).select(timeout.map(Err)).then(|res| {
+            match res {
+                Ok((Ok(pair), _timeout)) => Ok(pair),
+                Ok((Err(()), _handshake)) => {
+                    Err(other("timeout during handshake"))
+                },
+                Err((e, _other)) => Err(e),
+            }
+        }));
+
+        // shuttle data back and for between the two connections.
+        // data is read from c1 and written to c2
+        let buffer = self.buffer.clone();
+        mybox(pair.and_then(|(c1, c2)| {
+            let c1 = Rc::new(c1);
+            let c2 = Rc::new(c2);
+
+            let half1 = Transfer::new(c1.clone(), c2.clone(), buffer.clone());
+            let half2 = Transfer::new(c2, c1, buffer);
+            half1.join(half2)
+        }))
     }
 }
 
 fn mybox<F: Future + 'static>(f: F) -> Box<Future<Item=F::Item, Error=F::Error>> {
     Box::new(f)
 }
-
 
 fn other(desc: &str) -> io::Error {
     io::Error::new(io::ErrorKind::Other, desc)
